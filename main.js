3b2f3fdb3616e0983d16a2d47018e5b5b7339064
Grayson Kippes <GraysonKippes@my.unt.edu>
2026-03-10 12:53:10 -0500

    Added new task form.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, ipcMain } = require('electron/main')
+const { app, BrowserWindow, ipcMain, dialog } = require('electron/main')
 const path = require('node:path')
 
 const createWindow = () => {
--- renderer.js
+++ renderer.js
@@ -8,48 +8,32 @@ const func = async () => {
 
 func()
 
-/*document.getElementById('my-button').addEventListener('click', () => {
-  document.getElementById('output').innerText = "Button clicked!"
-})*/
-
-/*document.getElementById('task-name').innerText = "Assignment 1"
-document.getElementById('task-category').innerText = "CSCE 3444"
-document.getElementById('task-edit-button').addEventListener('click', () => {
-  document.getElementById('task-edit-form').removeAttribute('hidden')
+document.getElementById('add-new-task').addEventListener('click', () => {
+  let form = document.getElementById('task-edit-form');
+  if (form.hasAttribute('hidden')) {
+    form.removeAttribute('hidden');
+  }
 })
+
 document.getElementById('task-edit-form').addEventListener('submit', (event) => {
-  document.getElementById('task-name').innerText = document.getElementById('task-name-edit').innerText
-  document.getElementById('task-category').innerText = document.getElementById('task-category-edit').innerText
-})*/
+  event.preventDefault();
+  const name = event.target.elements[0].value;
+  const category = event.target.elements[1].value;
+  addTask(name, category);
+  event.target.setAttribute('hidden', 'hidden');
+  event.target.reset();
+})
 
-/*
-      <form id="task-edit-form">
-        <label for="task-name-edit">Task Name:</label>
-        <input type="text" id="task-name-edit" name="task-name-edit"><br><br>
-        <label for="task-category-edit">Task Name:</label>
-        <input type="text" id="task-category-edit" name="task-category-edit"><br><br>
-        <input type="submit" value="Submit">
-      </form>
-      <button id="task-edit-button">Edit</button>
-*/
-
-
-const div1 = document.createElement("div")
-const h2 = document.createElement("h2")
-const p = document.createElement("p")
-
-div1.id = "new-div"
-div1.className = "new-div-class"
-div1.textContent = "Hello World"
-div1.style.color = 'blue'
-div1.style.padding = '10px'
-
-h2.innerText = "Assignment 2"
-p.innerHTML = "CSCE 3444"
-//div1.appendChild(h2)
-//div1.appendChild(p)
-
-document.body.appendChild(div1)
-document.getElementById('my-div').appendChild(p)
-
-//console.log(document.body.childNodes)
+function addTask(name, category) {
+  const div1 = document.createElement("div");
+  const h2 = document.createElement("h2");
+  const p = document.createElement("p");
+  div1.id = "task-div-" + name;
+  div1.className = "task-div-class";
+  h2.innerText = name;
+  p.innerText = category;
+  div1.appendChild(h2);
+  div1.appendChild(p);
+  document.body.appendChild(div1);
+  console.log("Added new task to page.");
+}
