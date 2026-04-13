479272086a6eee4310e088c58a479287a89f76b4
AidanFawcett <aidanfawcett@my.unt.edu>
2026-04-13 11:08:06 -0500

    Removed redunantcies

--- preload.js
+++ preload.js
@@ -1,20 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
-contextBridge.exposeInMainWorld('versions', {
-  node: () => process.versions.node,
-  chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron,
-  ping: () => ipcRenderer.invoke('ping')
-})
-
 contextBridge.exposeInMainWorld('electronAPI', {
-  addTask: (name, category, priority) => ipcRenderer.send('add-task', name, category, priority),
+  addTask: (name, category, priority, dueDate) => ipcRenderer.send('add-task', name, category, priority, dueDate),
   removeTask: (name) => ipcRenderer.send('remove-task', name),
   addToCal: () => ipcRenderer.invoke('addToCal'),
-})
-
-//for changing between calendar and list view
-contextBridge.exposeInMainWorld('windowView', {
-  toggleCal: () => ipcRenderer.invoke('toggleCal'),
-  toggleList: () => ipcRenderer.invoke('toggleList')
 })
