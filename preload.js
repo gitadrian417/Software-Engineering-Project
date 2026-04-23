04e2d51d4fa9b60bed684ea951f9fda07271c25f
AidanFawcett <aidanfawcett@my.unt.edu>
2026-04-23 13:38:04 -0500

    Color variable properly added to channel

--- preload.js
+++ preload.js
@@ -1,8 +1,8 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  addTask: (name, category, priority, dueDate) => ipcRenderer.send('add-task', name, category, priority, dueDate),
-  editTask: (id, name, category, priority, dueDate) => ipcRenderer.send('edit-task', id, name, category, priority, dueDate),
+  addTask: (name, category, priority, dueDate, color) => ipcRenderer.send('add-task', name, category, priority, dueDate, color),
+  editTask: (id, name, category, priority, dueDate, color) => ipcRenderer.send('edit-task', id, name, category, priority, dueDate, color),
   removeTask: (id) => ipcRenderer.send('remove-task', id),
   getTasks: () => ipcRenderer.invoke('getTasks')
 })
